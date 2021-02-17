992a056d9808053afd2f6052eaa15ef542efeca9
Bartek <bartekma89@gmail.com>
2021-02-17 20:30:27 +0100

    refactored admin page, added useReducer to save users

--- src/screens/Admin/index.tsx
+++ src/screens/Admin/index.tsx
@@ -1,26 +1,68 @@
-import { useState, useEffect } from "react";
+import { useEffect, useReducer, Reducer } from "react";
 
 import { useDbFirebase } from "../../services/hooks";
 import { User } from "../../constants/types";
 import { UserList } from "./components";
 
+type StatusTypes = "idle" | "pending" | "resolved";
+type ActionTypes =
+  | {
+      type: "success";
+      users: User[] | [];
+    }
+  | {
+      type: "started";
+    };
+
+interface State {
+  status: StatusTypes;
+  users: User[] | [];
+}
+
+const initialState: State = {
+  status: "idle",
+  users: [],
+};
+
+const usersReducer: Reducer<State, ActionTypes> = (state, action) => {
+  switch (action.type) {
+    case "success":
+      return {
+        ...state,
+        status: "resolved",
+        users: action.users,
+      };
+    case "started":
+      return {
+        ...state,
+        status: "pending",
+      };
+    default: {
+      throw new Error("Unhandled action type");
+    }
+  }
+};
+
 export function AdminPage() {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [users, setUsers] = useState<User[] | []>([]);
+  const [state, dispatch] = useReducer(usersReducer, initialState);
 
   const db = useDbFirebase();
 
-  useEffect(() => {
-    setLoading(true);
+  const isLoading = state.status === "idle" || state.status === "pending";
+  const isResolved = state.status === "resolved";
 
+  useEffect(() => {
+    dispatch({ type: "started" });
     db.users().on("value", (snapshot) => {
       const usersObject = snapshot.val();
       const usersList = Object.keys(usersObject).map((key) => ({
         ...usersObject[key],
         uid: key,
       }));
-      setUsers(usersList);
-      setLoading(false);
+      dispatch({
+        type: "success",
+        users: usersList,
+      });
     });
 
     return () => db.users().off();
@@ -30,8 +72,8 @@ export function AdminPage() {
   return (
     <div>
       <h1>Admin</h1>
-      {loading && <div>Loading ...</div>}
-      <UserList users={users} />
+      {isLoading && <div>Loading ...</div>}
+      {isResolved && <UserList users={state.users} />}
     </div>
   );
 }
